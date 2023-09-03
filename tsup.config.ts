c8488c7bf8c8259cc5b1143450c30f1473e5fd35
YanAndFish <383904425@qq.com>
2023-09-03 14:24:35 +0800

    chore: update deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,27 @@
+import { writeFile } from 'node:fs/promises'
+import { basename, dirname } from 'node:path'
 import { defineConfig } from 'tsup'
+import glob from 'fast-glob'
+
+async function generateDeclareFile(sourcePath: string) {
+  const name = basename(sourcePath, '.ts')
+  const path = dirname(sourcePath).replace(/^src/, 'dist')
+
+  const content = `export * from './${path}/${name}'\nexport { default } from './${path}/${name}'`
+  await writeFile(`./${name}.d.ts`, content)
+}
+
+const entryPoints = ['src/index.ts', 'src/presets/*.ts']
 
 export default defineConfig({
   splitting: false,
   dts: true,
   clean: true,
   cjsInterop: true,
-  entryPoints: ['src/presets/yafh.ts'],
+  entryPoints,
   format: ['cjs', 'esm'],
+  onSuccess: async () => {
+    const entrys = await glob(entryPoints, { onlyFiles: true })
+    await Promise.all(entrys.map(generateDeclareFile))
+  },
 })
